b47283f2874631ca993404547a8b72a21b1345a7
Soumojit777 <poddarsoumojit777@gmail.com>
2025-10-20 10:11:53 +0530

    update middleware

--- middleware.js
+++ middleware.js
@@ -1,4 +1,4 @@
-import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
+import arcjet, { createMiddleware, shield } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
@@ -16,14 +16,6 @@ const aj = arcjet({
     // Shield protection for content and security
     shield({
       mode: "LIVE",
-    }),
-    detectBot({
-      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
-      allow: [
-        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
-        "GO_HTTP", // For Inngest
-        // See the full list at https://arcjet.com/bot-list
-      ],
     }),
   ],
 });
